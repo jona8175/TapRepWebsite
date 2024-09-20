0eb56de61f8091183d30a1ba456545c0ecd89dbf
jona8175 <jonas.kubik@uzh.ch>
2024-09-20 17:16:13 +0200

    TheeJs Scene aufgesetzt: Loadingmanager, Backgound, Models (GLTF), Lights, Controles, Renderer, Sizes (window), tick.

--- src/routes/blockdiagram/+page.svelte
+++ src/routes/blockdiagram/+page.svelte
@@ -1,50 +1,156 @@
 <script>
   import { browser } from "$app/environment";
   import * as THREE from "three";
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
+
+  import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+  //import { gsap } from "gsap";
+
   let root;
   onMount(() => {
     const canvas = root.querySelector(".webgl");
     // Do something with nd, such as adding event listeners, styles, etc.
 
     if (browser) {
+      const loadingManager = new THREE.LoadingManager(
+        // Loaded
+        () => {
+          // Wait a little
+          window.setTimeout(() => {
+            // Animate overlay
+            gsap.to(overlayMaterial.uniforms.uAlpha, {
+              duration: 3,
+              value: 0,
+              delay: 1,
+            });
+
+            // Update loadingBarElement
+            loadingBarElement.classList.add("ended");
+            loadingBarElement.style.transform = "";
+          }, 500);
+
+          window.setTimeout(() => {
+            sceneReady = true;
+          }, 2000);
+        },
+
+        // Progress
+        (itemUrl, itemsLoaded, itemsTotal) => {
+          // Calculate the progress and update the loadingBarElement
+          const progressRatio = itemsLoaded / itemsTotal;
+          loadingBarElement.style.transform = `scaleX(${progressRatio})`;
+        }
+      );
+
+      const gltfLoader = new GLTFLoader(loadingManager);
+      const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
 
       const scene = new THREE.Scene();
+
+      /**
+       * Background
+       */
+      const params = {
+        color: "#ffffff",
+      };
+      scene.background = new THREE.Color(params.color);
+
+      /**
+       * Models
+       */
+
+      gltfLoader.load("/models/BlockDiagram.gltf", (gltf) => {
+        gltf.scene.scale.set(1, 1, 1);
+        gltf.scene.rotation.set(0, 0, 0);
+        scene.add(gltf.scene);
+
+        updateAllMaterials();
+      });
+
       const camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
+      /**
+       * Lights
+       */
+      const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+      directionalLight.castShadow = true;
+      directionalLight.shadow.camera.far = 15;
+      directionalLight.shadow.mapSize.set(1024, 1024);
+      directionalLight.shadow.normalBias = 0.05;
+      directionalLight.position.set(0.25, 3, -2.25);
+      scene.add(directionalLight);
+
+      // Controls
+      const controls = new OrbitControls(camera, canvas);
+      controls.enableDamping = true;
+
+      /**
+       * Renderer
+       */
 
       const renderer = new THREE.WebGLRenderer({
         canvas: canvas,
         antialias: true,
       });
 
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.setAnimationLoop(animate);
+      renderer.toneMapping = THREE.ReinhardToneMapping;
+      renderer.toneMappingExposure = 3;
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      
+
+      /**
+       * Sizes
+       */
+
+      const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+
+      window.addEventListener("resize", () => {
+        // Update sizes
+        sizes.width = window.innerWidth;
+        sizes.height = window.innerHeight;
+
+        // Update camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
+
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      });
 
       camera.position.z = 5;
 
-      function animate() {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+      const tick = () => {
+        controls.update()
+        console.log("hey")
+        if (true) {
+          // TODO: Implement points and Loading
+        }
+        renderer.render(scene, camera)
+        window.requestAnimationFrame(tick)
+      };
+      tick()
 
-        renderer.render(scene, camera);
-      }
     }
   });
 </script>
-<div bind:this={root}>
-    <canvas class= "webgl"></canvas>
-</div>
 
+<div bind:this={root}>
+  <canvas class="webgl"></canvas>
+</div>
 
 <style>
   .webgl {
